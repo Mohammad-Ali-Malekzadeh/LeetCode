90b372e72211a62b581164244079827001207d1b
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-01-02 23:06:44 +0330

    Reverse-String-V1 (py)

---
+++ Reverse-String/app.py
@@ -0,0 +1,8 @@
+#! V1 - 205 ms 💔
+""" 
+.reverse() python builtin mehton is used in this solution
+"""
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        return s.reverse()

9ef2fc4fe2f319a02fc4b7742e206fdf8fd5e1f7
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-01-05 00:00:01 +0330

    v2 of reverse-string

--- Reverse-String/app.py
+++ Reverse-String/app.py
@@ -1,8 +1,16 @@
 #! V1 - 205 ms 💔
-""" 
-.reverse() python builtin mehton is used in this solution
-"""
+#? .reverse() python builtin mehton is used in this solution
 
 class Solution:
     def reverseString(self, s: list[str]) -> None:
         return s.reverse()
+    
+#! V2 - ? ms 😎
+#? list[x::y] Used
+
+    class Solution:
+        def reverseString(self, s: list[str]) -> None:
+            string = ''
+            for i in s:
+                string += i
+            return list(string[::-1])

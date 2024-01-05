e4078f7248a42ba0e6ed2e70dbe3e8727f1e4a63
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-01-05 14:27:36 +0330

    Reverse-String-V3&4 (py)

--- Reverse-String/app.py
+++ Reverse-String/app.py
@@ -5,12 +5,32 @@ class Solution:
     def reverseString(self, s: list[str]) -> None:
         return s.reverse()
     
-#! V2 - ? ms 😎
-#? list[x::y] Used
+#! V2 - 961 ms 🥲
+#? Loop on the list. add the first index on the end of the array then delete it.
 
-    class Solution:
-        def reverseString(self, s: list[str]) -> None:
-            string = ''
-            for i in s:
-                string += i
-            return list(string[::-1])
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        for i in range(len(s)):
+            if i == 0:
+                s.insert(len(s), s[0])
+            else:
+                s.insert(-1 - (i -1), s[0])
+            s.pop(0)
+        return s
+    
+#! V3 - Not accept in Site
+#? String[x::y] Used
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        string = ''
+        for i in s:
+            string += i
+        return list(string[::-1])
+
+#! V4 - Not accept in Site
+#? List[x::y] Used
+    
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        print(s[::-1])

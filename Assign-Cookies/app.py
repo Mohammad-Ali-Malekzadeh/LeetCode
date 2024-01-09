f98806789869a59ceba656406e51635db3cf0ac8
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-01-09 17:34:58 +0330

    Assign-Cookies-V1-fixed

--- Assign-Cookies/app.py
+++ Assign-Cookies/app.py
@@ -1,8 +1,12 @@
 class Solution:
     def findContentChildren(self, g: list[int], s: list[int]) -> int:
         result = 0
-        for i in s:
-            if i in g:
-                del g[g.index(i)]
-                result += 1
+        s = sorted(s)
+        g = sorted(g)
+        for i in range(len(g)):
+            for j in range(len(s)):
+                if s[j] >= g[i]:
+                    s.pop(j)
+                    result += 1
+                    break
         return result

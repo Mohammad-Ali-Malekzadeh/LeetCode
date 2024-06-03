3e6c4c364052f3cebfcf82ac56d89033fca9c321
Mohammad-Ali-Malekzadeh <75009952+Mohammad-Ali-Malekzadeh@users.noreply.github.com>
2024-06-03 22:35:31 +0330

    update challenge files

--- 796 -Rotate-String/app.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        quality = ''
-        for i in range(0, len(s)):
-            quality = s[i+1:] + s[:i+1]
-            
-            if quality == goal:
-                return True
-        return False   
